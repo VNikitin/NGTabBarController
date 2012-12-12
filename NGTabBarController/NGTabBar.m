8379ca25365e4c77c3164284bce020de242c10fd
SubMarine <vnikitin@mistralservice.ru>
2012-12-12 11:36:11 +0400

    save

--- NGTabBarController/NGTabBar.m
+++ NGTabBarController/NGTabBar.m
@@ -205,7 +205,8 @@
 
 - (void)setItems:(NSArray *)items {
     if (items != _items) {
-        [_items performSelector:@selector(removeFromSuperview)];
+//        TODO: check in other branch
+        [_items makeObjectsPerformSelector:@selector(removeFromSuperview)];
         
         _items = items;
         
